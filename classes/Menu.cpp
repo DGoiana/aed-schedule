78358e814dbf322a02ff828d83e5fc25a272f59b
rubuy-74 <rubemviscard2635@gmail.com>
2023-10-20 09:32:44 +0100

    Resolve minor mistake

--- classes/Menu.cpp
+++ classes/Menu.cpp
@@ -9,7 +9,7 @@ Menu::Menu(string input, vector<string> menuOptions){
 }
 
 void Menu::printMenu(){
-    for(int i = 0; i < menuOptions.size(); i++){
+    for(size_t i = 0; i < menuOptions.size(); i++){
         cout << i << '-' << menuOptions.at(i) << '\n';
     }
     cout << inputPrompt;
