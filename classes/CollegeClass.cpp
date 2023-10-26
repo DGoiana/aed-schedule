29cc87125e16a2d6c88375ec727e80b9f8c3b997
DGoiana <diogogoianam@gmail.com>
2023-10-26 18:02:34 +0100

    Joining two classes

--- classes/CollegeClass.cpp
+++ classes/CollegeClass.cpp
@@ -4,7 +4,8 @@ CollegeClass::CollegeClass(){
     this->classCode = "";
 }
 
-CollegeClass::CollegeClass(string classCode){
+CollegeClass::CollegeClass(string classCode, string ucCode){
+    this->ucCode = ucCode;
     this->classCode = classCode;
 }
 
@@ -12,6 +13,24 @@ string CollegeClass::get_classCode() const{
     return this->classCode;
 }
 
+string CollegeClass::get_ucCode() const{
+    return this->ucCode;
+}
+
 void CollegeClass::set_classCode(const string& new_classCode){
     this->classCode = new_classCode;
+}
+
+void CollegeClass::set_ucCode(const string& new_ucCode){
+    this->ucCode = new_ucCode;
+}
+
+bool CollegeClass::operator==(CollegeClass college_class) const {
+    return this->ucCode == college_class.get_ucCode() && this->classCode == college_class.get_classCode();
+}
+
+bool CollegeClass::operator<(CollegeClass college_class) const {
+    string toCompare = college_class.get_ucCode() + college_class.get_classCode();
+    string thisToCompare = ucCode + classCode;
+    return thisToCompare < toCompare;
 }
--- classes/CollegeClass.h
+++ classes/CollegeClass.h
@@ -9,12 +9,17 @@ using namespace std;
 class CollegeClass {
     private:
         string classCode;
+        string ucCode;
         // if parsing through all students classes takes too long, create a list of students
     public:
         CollegeClass();
-        CollegeClass(string classCode);
+        CollegeClass(string classCode, string ucCode);
         string get_classCode() const;
+        string get_ucCode() const;
         void set_classCode(const string& new_classCode);
+        void set_ucCode(const string& new_ucCode);
+        bool operator==(CollegeClass college_class) const;
+        bool operator<(CollegeClass college_class) const;
 };
 
 #endif
